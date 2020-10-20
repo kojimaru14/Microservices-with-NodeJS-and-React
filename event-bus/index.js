ab4deb2a6dbb30a9cbb0604176e75b7a13eedf4f
jiko <>
2020-10-21 08:52:11 +0900

    Dockerizing services (Comments, Moderation and Query) - except for Client (service for frontend) https://box.udemy.com/course/microservices-with-node-js-and-react/learn/lecture/19099844

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,10 +11,10 @@ app.post('/events', (req, res) => {
     const event = req.body;
     events.push(event);
 
-    axios.post('http://posts-clusterip-srv:4000/events', event); // posts service
-    // axios.post('http://localhost:4001/events', event); // comments service
-    // axios.post('http://localhost:4002/events', event); // query service
-    // axios.post('http://localhost:4003/events', event); // moderation service
+    axios.post('http://posts-clusterip-srv:4000/events', event);// posts service
+    axios.post('http://comments-srv:4001/events', event);       // comments service
+    axios.post('http://query-srv:4002/events', event);          // query service
+    axios.post('http://moderation-srv:4003/events', event);     // moderation service
 
     res.send({status: 'OK'});
 });
