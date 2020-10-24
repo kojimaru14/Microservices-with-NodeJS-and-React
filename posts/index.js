3d39dfc2400af3a3a0ae5f38567c5ac360105ad8
jiko <>
2020-10-24 15:31:11 +0900

    Adding ingress service and configuring unique route paths

--- posts/index.js
+++ posts/index.js
@@ -10,11 +10,12 @@ app.use(cors());
 
 const posts = {};
 
+// This route handler is no longer used.
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', async(req, res) => {
+app.post('/posts/create', async(req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
